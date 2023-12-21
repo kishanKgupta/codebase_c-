b9d3288f1b1818468ee6655861fe5e07d1631566
Kishan Kumar Gupta <142384429+kishanKgupta@users.noreply.github.com>
2023-12-21 05:36:39 +0000

    feature

---
+++ string/string1.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string.h>
+//#include <cctype>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    cout<<"char and string"<<endl;
+    cout<<"c++ best for competitive programming"<<endl;
+
+  //  char name[100];
+   // cin>>name;
+   // cout<<"Name is "<<name; // print the value came before the wightspace
+//     cin.getline(name,50);
+//    cout<<"Name is "<<name<<endl;
+//    cout<<strlen(name)<<endl;
+   string name;
+   //cin>>name;
+   //cout<<"name is "<<name<<endl; //kishan gupta => kishan
+  
+   // getline(cin,name);
+  // transform(name.begin(), name.end(), name.begin(), ::toupper);
+   // cout<<"name is "<<name<<endl;
+
+   return 0;
+}
